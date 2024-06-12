88e9f0f7578a5d17c5a2607ded5ab93621110394
syayid muhammad akbar <114471222+syayidmuhammadakbar@users.noreply.github.com>
2024-06-12 08:41:59 +0000

    define model

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import streamlit as st
-from txtai.pipeline import Summary
+from transformers import pipeline
 from PyPDF2 import PdfFileReader
-
-
 
 st.set_page_config(layout='wide')
 
 @st.cache_resource
 def summary_text(text):
-    summary = Summary()
-    text = (text)
-    result = summary(text)
-    return result
+    summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+    result = summarizer(text)
+    return result[0]['summary_text']
 
 # Extract text from the PDF file using pyPDF2
 def extract_text_from_pdf(file_path):
@@ -28,7 +25,7 @@ if choice == 'Summarize Text':
     st.text('Ini adalah website _text summary_  yang dapat memudahkan dalam merangkum sebuah kalimat.')
     st.text('Project ini disusun oleh: Muhammad Reski Djunaedi, Muhammad Rofiul Arham, Syayid Muhammad Akbar, Sintia Sari, Reva Aulia Faradilah')
     input_text = st.text_area('Enter your text here !')
-    if input_text is not None:
+    if input_text:
         if st.button('Summarize Text'):
             col1, col2 = st.columns([1,1])
             with col1:
@@ -37,7 +34,7 @@ if choice == 'Summarize Text':
             with col2:
                 result = summary_text(input_text)
                 st.markdown("***Summary***")
-                st.success(result)\
-                
+                st.success(result)
+
                 # Add reference summary for ROUGE evaluation
-                reference_summary = (input_text)
+                reference_summary = input_text
